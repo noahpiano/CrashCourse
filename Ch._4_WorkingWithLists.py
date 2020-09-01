166f36ddadcd3e2e2008a7a9bc005622cc0b8237
NBALLOU <noahpiano@gmail.com>
2020-09-01 19:25:38 -0400

    this is it

---
+++ Ch._4_WorkingWithLists.py
@@ -0,0 +1,6 @@
+# 9/1/2020
+print('Looping Through Lists')
+magicians = ['alice', 'david', 'carolina']  # this is a for loop
+for magician in magicians:  # this tells Python to pull a name from the list
+    #                   'magicians' and associate it with the variable 'magician'
+    print(magician)  # will print all elements in the list
